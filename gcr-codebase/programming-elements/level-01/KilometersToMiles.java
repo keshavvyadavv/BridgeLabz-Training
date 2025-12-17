af51fb39a8389e9447482335f82a8090764336d3
keshavvyadavv <keshavyadavv21@gmail.com>
2025-12-17 13:12:02 +0530

    [Keshav Yadav] Added kilometer to mile program.

---
+++ gcr-codebase/programming-elements/level-01/KilometersToMiles.java
@@ -0,0 +1,14 @@
+// Create KilometersToMiles Class to compute kilometer to mile.
+public class KilometersToMiles{
+	public static void main(String[] args){
+
+	    // Create variable kilometerDistance for distance in miles.
+	    double kilometerDistance = 10.8;
+	    
+	    // Create variable mileDistance for distance in kilometers.
+	    double mileDistance = 10.8 * 1.6;
+		
+	    // Print the distance in kilometer and in mile distance.
+	    System.out.println("The distance " +kilometerDistance+ "km in miles is " +mileDistance);
+	}
+}
