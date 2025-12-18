33ee589c3dcd74a47795cd1351eefe6b9e5b592b
keshavvyadavv <keshavyadavv21@gmail.com>
2025-12-19 00:09:09 +0530

    [Keshav Yadav]: Added calculator using switch case program.

--- gcr-codebase/control-flow/level-03/CalculatorUsingSwitchCase.java
+++ gcr-codebase/control-flow/level-03/CalculatorUsingSwitchCase.java
@@ -1,46 +1,44 @@
 // Create CalculatorUsingSwitchCase class that calculate using switch-case
 import java.util.Scanner;
-
 public class CalculatorUsingSwitchCase {
     public static void main(String[] args) {
 
-        // Create Scanner object to take input and take first, 
-		// second numberand operator as input
-		double firstNumber;
+        // Create Scanner object to take input
+        double firstNumber;
         double secondNumber;
         String operator;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter first number:");
-        first = input.nextDouble();
+        firstNumber = input.nextDouble();
         System.out.println("Enter second number:");
-        second = input.nextDouble();
+        secondNumber = input.nextDouble();
         System.out.println("Enter operator (+, -, *, /):");
         operator = input.next();
-
-        // Switch-case to for calculation
-        switch (op) {
+		
+        // Switch-case for calculation
+        switch (operator) {
             case "+":
-                System.out.println("Result = " + (first + second) );
+                System.out.println("Result = " + (firstNumber + secondNumber));
                 break;
 
             case "-":
-                System.out.println("Result = " + (first - second));
+                System.out.println("Result = " + (firstNumber - secondNumber));
                 break;
 
             case "*":
-                System.out.println("Result = " + (first * second ));
+                System.out.println("Result = " + (firstNumber * secondNumber));
                 break;
 
             case "/":
-                if (second != 0) {
-                    System.out.println("Result = " + (first / second ));
+                if (secondNumber != 0) {
+                    System.out.println("Result = " + (firstNumber / secondNumber));
                 } else {
-                    System.out.println("Division by zero is not allowed. ");
+                    System.out.println("Division by zero is not allowed.");
                 }
                 break;
 
             default:
-                System.out.println("Operator is not valid ");
+                System.out.println("Operator is not valid");
         }
         input.close();
     }
