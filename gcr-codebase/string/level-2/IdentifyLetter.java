3dff325d3feed15ef99737715ca360ee6f338880
keshavvyadavv <keshavyadavv21@gmail.com>
2025-12-22 17:01:00 +0530

    [Keshav Yadav]: Added identify letter program.

--- gcr-codebase/string/level-2/IdentifyLetter.java
+++ gcr-codebase/string/level-2/IdentifyLetter.java
@@ -13,16 +13,29 @@ public class IdentifyLetter{
 		System.out.println(ch);
 		return "Not a letter";
 	}
+	public static int[] countVowelOrConsonant(String string){
+		int vowelCount = 0;
+		int consonantCount = 0;
+		for(int index = 0 ; index < string.length() ; index++){
+			String result = checkVowelOrConsonant(string.charAt(index));
+			if(result.equals("Vowel")){
+				vowelCount++;
+			}
+			else if(result.equals("Consonant")){
+				consonantCount++;
+			}
+		}
+		return new int[] {vowelCount , consonantCount};
+	}
 	public static void main(String [] args){
 		
 		//create scanner object 
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter the character: ");
-		char ch = input.next().charAt(0);
-		String result = checkVowelOrConsonant(ch);
-		
+		System.out.println("Enter the String: ");
+		String string = input.nextLine();
+		int [] result = countVowelOrConsonant(string);
 		//display display
-		System.out.println(result);
+		System.out.println("Count of vowel: "+result[0] + ", Count of consonant: " + result[1]);
 		input.close();
 	}
 }
