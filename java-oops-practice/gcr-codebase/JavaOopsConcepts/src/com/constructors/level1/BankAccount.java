9d504e0ae355d3e41f7c2d9c5462c427c4b25737
keshavvyadavv <keshavyadavv21@gmail.com>
2025-12-29 11:06:25 +0530

    [Keshav Yadav]: modified saving and bank account program.

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/level1/BankAccount.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/level1/BankAccount.java
@@ -1,5 +1,27 @@
 package com.constructors.level1;
 
+//Parent class
 public class BankAccount {
+ public int accountNumber;
+ protected String accountHolder;  
+ private double balance;          
 
+ // Constructor
+ public BankAccount(int accountNumber, String accountHolder, double balance) {
+     this.accountNumber = accountNumber;
+     this.accountHolder = accountHolder;
+     this.balance = balance;
+ }
+
+ //Public method to access balance
+ public double getBalance() {
+     return balance;
+ }
+
+ //Public method to modify balance
+ public void setBalance(double balance) {
+     if (balance >= 0) {
+         this.balance = balance;
+     }
+ }
 }
--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/level1/SavingsAccount.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/level1/SavingsAccount.java
@@ -1,5 +1,17 @@
 package com.constructors.level1;
 
-public class SavingsAccount {
+//Subclass
+public class SavingsAccount extends BankAccount {
 
+ // Constructor
+ public SavingsAccount(int accountNumber, String accountHolder, double balance) {
+     super(accountNumber, accountHolder, balance);
+ }
+
+ // Method to demonstrate access
+ public void displayDetails() {
+     System.out.println("Account Number: " + accountNumber);  
+     System.out.println("Account Holder: " + accountHolder);   
+     System.out.println("Balance: " + getBalance());            
+ }
 }
