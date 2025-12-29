95ae38f4b85e722b16108a467f22c6c3e9525acd
keshavvyadavv <keshavyadavv21@gmail.com>
2025-12-30 00:04:49 +0530

    [Keshav Yadav]: Added company program.

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/javakeywords/level1/Company.java
@@ -0,0 +1,68 @@
+package com.javakeywords.level1;
+
+public class Company {
+
+	   // Company details
+	   private static String companyName;
+	   private static int totalEmployees = 0;
+
+	   private String name;
+	   private final int id;
+	   private String designation;
+
+	   // Constructor
+	   public Company(String name, int id, String designation) {
+	       this.name = name;
+	       this.id = id;
+	       this.designation = designation;
+	       totalEmployees++;
+	   }
+
+	   // Sets company name
+	   public static void setCompanyName(String name) {
+	       companyName = name;
+	   }
+
+	   // Shows total employees
+	   public static void displayTotalEmployees() {
+	       System.out.println("Total Employees: " + totalEmployees);
+	   }
+
+	   // Displays employee info
+	   public void displayEmployeeDetails() {
+	       if (this instanceof Company) {
+	           System.out.println("Company Name: " + companyName);
+	           System.out.println("Employee ID: " + id);
+	           System.out.println("Name: " + name);
+	           System.out.println("Designation: " + designation);
+	       } else {
+	           System.out.println("Invalid object.");
+	       }
+	   }
+
+	   // Getters
+	   public String getName() {
+	       return name;
+	   }
+
+	   public int getId() {
+	       return id;
+	   }
+
+	   public String getDesignation() {
+	       return designation;
+	   }
+
+	   public static void main(String[] args) {
+
+	       Company.setCompanyName("Tech Solutions Inc.");
+
+	       Company emp1 = new Company("Thamarai", 101, "Software Engineer");
+	       Company emp2 = new Company("Rohan", 102, "Project Manager");
+
+	       Company.displayTotalEmployees();
+
+	       emp1.displayEmployeeDetails();
+	       emp2.displayEmployeeDetails();
+	   }
+	}
