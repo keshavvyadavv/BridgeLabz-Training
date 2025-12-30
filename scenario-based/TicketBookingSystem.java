9b31b581ea1a47ff02abe93e7fc469ac6f84788d
keshavvyadavv <keshavyadavv21@gmail.com>
2025-12-30 12:38:10 +0530

    [Keshav Yadav]: Modified ticker booking system program.

--- scenario-based/TicketBookingSystem.java
+++ scenario-based/TicketBookingSystem.java
@@ -17,32 +17,34 @@ public class TicketBookingSystem{
 		//Welcome message
 		System.out.println("Welcome to the ticket counter.");
 		while(true){
-		//Taking distance form user
-			System.out.println("Enter distance in km: ");
-			double distance = input.nextDouble();
-			System.out.println("Enter the station from: ");
-			String stationFrom = input.next();
-			System.out.println("Enter the station to: ");
-			String stationTo = input.next();
-			
-			int amount =0;
-			if(distance>0 && distance <=100){
-				amount =  150;
-			}else if(distance>100 && distance <300){
-				amount = 250;
-			}else if (distance <750){
-				amount = 700;
-			}else if(distance <1500){
-				amount = 1500;
-			}else{
-				amount = 25000;
-			}
-			System.out.println ("Choose 1 to book ticket \n2 to exit \n 3 to check the number of seats left.");
+		
+			System.out.println ("Choose 1 to book ticket \n2 to exit \n3 to check the number of seats left.");
 			int choose = input.nextInt();
 			//switch case
 			switch(choose){
-				case 1: System.out.println("Tickect booked successfully and billed amount is: " + amount);
-						System.out.println("From "+ stationFrom + " to station " + stationTo);
+				case 1:
+					//Taking distance form user
+					System.out.println("Enter distance in km: ");
+					double distance = input.nextDouble();
+					System.out.println("Enter the station from: ");
+					String stationFrom = input.next();
+					System.out.println("Enter the station to: ");
+					String stationTo = input.next();
+					
+					int amount =0;
+					if(distance>0 && distance <=100){
+						amount =  150;
+					}else if(distance>100 && distance <300){
+						amount = 250;
+					}else if (distance <750){
+						amount = 700;
+					}else if(distance <1500){
+						amount = 1500;
+					}else{
+						amount = 25000;
+					}
+					System.out.println("Tickect booked successfully and billed amount is: " + amount);
+					System.out.println("From "+ stationFrom + " to station " + stationTo);
 					occupied++;
 					break;
 				
@@ -57,8 +59,6 @@ public class TicketBookingSystem{
 				System.exit(0);
 			}
 		}
-		
-		
 		
 	}
 }
