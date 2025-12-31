50af7821f17ebda7b5e7d5977404e008eac9df37
keshavvyadavv <keshavyadavv21@gmail.com>
2025-12-31 14:55:18 +0530

    [Keshav Yadav]: Added vehicle program.

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/level1/Truck.java
@@ -0,0 +1,14 @@
+package com.objectmodeling.level1;
+//create Truck class that inherite vehicle using extends
+public class Truck extends Vehicle {
+	private int loadingCapacity;
+	public void displayInfo() {
+		super.displayInfo();
+		System.out.println("The loading capacity(Load) is: "+loadingCapacity+"\n");
+	}
+	Truck(int maxSpeed , String fuelType, int loadingCapacity){
+		super(maxSpeed , fuelType);
+		this.loadingCapacity = loadingCapacity;
+	}
+}
+
