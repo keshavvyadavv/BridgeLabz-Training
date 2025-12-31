ff10d757030b24ede858420cae8091c7d3eea2bf
keshavvyadavv <keshavyadavv21@gmail.com>
2025-12-31 13:04:35 +0530

    [Keshav Yadav]: Added animal dog cat relation program.

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/level1/Animal.java
@@ -0,0 +1,14 @@
+package com.inheritance.level1;
+
+public class Animal {
+	private String name;
+	private int age;
+	
+	public Animal(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
+	public void makeSound() {
+		System.out.println("");
+	}
+}
