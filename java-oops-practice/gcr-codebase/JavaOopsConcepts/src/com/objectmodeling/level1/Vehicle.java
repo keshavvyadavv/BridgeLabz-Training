7594d6f860b93cec272c5092212a7ed2534bebd0
keshavvyadavv <keshavyadavv21@gmail.com>
2025-12-31 14:42:28 +0530

    [Keshav Yadav]: Added vehicle program.

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/level1/Vehicle.java
@@ -0,0 +1,14 @@
+package com.objectmodeling.level1;
+//create Vehicle class that act as a super class
+public class Vehicle {
+	private int maxSpeed;
+	private String fuelType;
+	public void displayInfo() {
+		System.out.println("The speed of vehicle is: "+maxSpeed
+							+"\nThat run's on "+fuelType);
+	}
+	Vehicle(int maxSpeed , String fuelType){
+		this.maxSpeed = maxSpeed;
+		this.fuelType = fuelType;
+	}
+}
