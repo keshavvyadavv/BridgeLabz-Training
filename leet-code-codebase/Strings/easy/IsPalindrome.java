2297a0c9fe8ac2bd291ad6bc946a54613854679b
keshavvyadavv <keshavyadavv21@gmail.com>
2026-01-01 11:17:50 +0530

    [Keshav Yadav]: Added palindromic string program.

---
+++ leet-code-codebase/Strings/easy/IsPalindrome.java
@@ -0,0 +1,26 @@
+public class IsPalindrome {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right){
+            char leftChar = Character.toLowerCase(s.charAt(left));
+            char rightChar = Character.toLowerCase(s.charAt(right));
+            if(!Character.isLetterOrDigit(leftChar)){
+                left++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(rightChar)){
+                right--;
+                continue;
+            }
+            if(leftChar != rightChar){
+                return false;
+            }
+            left++;
+            right--;
+            
+        }
+        return true;
+    }
+}
