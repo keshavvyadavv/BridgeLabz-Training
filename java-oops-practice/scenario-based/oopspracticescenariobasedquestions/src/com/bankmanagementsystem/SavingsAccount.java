1e03f49c629f0620f6591ee0d76788d4bc5d554e
keshavvyadavv <keshavyadavv21@gmail.com>
2026-01-02 11:12:34 +0530

    [Keshav Yadav]: Added bankmanagement program.

---
+++ java-oops-practice/scenario-based/oopspracticescenariobasedquestions/src/com/bankmanagementsystem/SavingsAccount.java
@@ -0,0 +1,19 @@
+package com.bankmanagementsystem;
+
+public class SavingsAccount extends Account
+{
+	private double interestRate;
+
+	public SavingsAccount(double balance, double interestRate)
+	{
+		super(balance);
+		this.interestRate = interestRate;
+	}
+
+	public void calculateInterest()
+	{
+		double interest = balance * interestRate / 100;
+		balance += interest;
+		System.out.println("Interest Added: ₹" + interest);
+	}
+}
