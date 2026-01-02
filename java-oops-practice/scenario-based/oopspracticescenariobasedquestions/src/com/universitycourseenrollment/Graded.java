f7208436f89f52ff0c7131335a7143cedb57ff61
keshavvyadavv <keshavyadavv21@gmail.com>
2026-01-02 15:41:23 +0530

    [Keshav Yadav]: Added university system program.

---
+++ java-oops-practice/scenario-based/oopspracticescenariobasedquestions/src/com/universitycourseenrollment/Graded.java
@@ -0,0 +1,5 @@
+package com.universitycourseenrollment;
+
+public interface Graded {
+    void assignGrade(String grade);
+}
---
+++ java-oops-practice/scenario-based/oopspracticescenariobasedquestions/src/com/universitycourseenrollment/Postgraduate.java
@@ -0,0 +1,12 @@
+package com.universitycourseenrollment;
+
+public class Postgraduate extends Student {
+
+    public Postgraduate(int studentId, String name) {
+        super(studentId, name);
+    }
+
+    public void calculateGPA(double totalPoints, int subjects) {
+        setGPA((totalPoints / subjects) + 0.5);
+    }
+}
