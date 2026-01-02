8ff226e5d59c94f8b799c48040fafb5d80e91f14
keshavvyadavv <keshavyadavv21@gmail.com>
2026-01-02 16:03:15 +0530

    [Keshav Yadav]: Added wallet application program.

---
+++ java-oops-practice/scenario-based/oopspracticescenariobasedquestions/src/com/ewalletapplication/BusinessWallet.java
@@ -0,0 +1,22 @@
+package com.ewalletapplication;
+
+public class BusinessWallet extends Wallet {
+
+    public BusinessWallet() {
+        super(0);
+    }
+
+    public void transferTo(User receiver, double amount) {
+        double tax = amount * 0.02; // 2% tax
+        double total = amount + tax;
+
+        if (total <= getBalance()) {
+            deductBalance(total);
+            receiver.getWallet().addBalance(amount);
+            history.add(new Transaction("Business Transfer", amount));
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+}
+
