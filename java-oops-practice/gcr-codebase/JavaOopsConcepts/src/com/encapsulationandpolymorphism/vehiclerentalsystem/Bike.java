c0352efdf9a7ef47f1a936daa196fe8572c72be6
keshavvyadavv <keshavyadavv21@gmail.com>
2026-01-02 11:32:48 +0530

    [Keshav Yadav]: Added vehiclerendtal system program.

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/vehiclerentalsystem/Bike.java
@@ -0,0 +1,27 @@
+package com.encapsulationandpolymorphism.vehiclerentalsystem;
+
+public class Bike extends Vehicle implements Insurable {
+
+	// Constructor
+	public Bike(String vehicleNumber, double rentalRate, String insurancePolicyNumber) {
+		super(vehicleNumber, "Bike", rentalRate, insurancePolicyNumber);
+	}
+
+	// Rental cost calculation for bike
+	@Override
+	public double calculateRentalCost(int days) {
+		return days * getRentalRate();
+	}
+
+	// Insurance calculation for bike
+	@Override
+	public double calculateInsurance() {
+		return 500;
+	}
+
+	// Insurance details
+	@Override
+	public String getInsuranceDetails() {
+		return "Bike Insurance | Policy: " + getMaskedPolicyNumber();
+	}
+}
