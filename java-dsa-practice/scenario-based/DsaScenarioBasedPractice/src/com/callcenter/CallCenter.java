3867a663a5f22b81cead91c4f4aedfbb4f2e2a6d
keshavvyadavv <keshavyadavv21@gmail.com>
2026-01-12 13:26:56 +0530

    [Keshav Yadav]: Update CallCenter.java

--- java-dsa-practice/scenario-based/DsaScenarioBasedPractice/src/com/callcenter/CallCenter.java
+++ java-dsa-practice/scenario-based/DsaScenarioBasedPractice/src/com/callcenter/CallCenter.java
@@ -5,23 +5,18 @@ class CallCenter {
 
     // for normal customer queue 
     private Queue<Customer> normalQueue = new LinkedList<>();
-
     // priority queue for VIP customer  
-    private PriorityQueue<Customer> vipQueue =
-            new PriorityQueue<>((a, b) -> a.customerId.compareTo(b.customerId));
-
+    private PriorityQueue<Customer> vipQueue = new PriorityQueue<>((a, b) -> a.customerId.compareTo(b.customerId));
     // Tracks number of calls per customer
     private HashMap<String, Integer> callCountMap = new HashMap<>();
 
     //Add incoming call
     public void receiveCall(Customer customer) {
-
         //update call count
         callCountMap.put(
                 customer.customerId,
                 callCountMap.getOrDefault(customer.customerId, 0) + 1
         );
-
         //add to appropriate queue
         if (customer.isVIP) {
             vipQueue.add(customer);
@@ -35,7 +30,6 @@ class CallCenter {
     //process next call
     public void processCall() {
         Customer customer;
-
         if (!vipQueue.isEmpty()) {
             customer = vipQueue.poll();
             System.out.println("Processing VIP customer: " + customer);
@@ -50,7 +44,7 @@ class CallCenter {
 
     // show call statistics
     public void showCallStatistics() {
-        System.out.println("\n📊 Monthly Call Count:");
+        System.out.println("\n Monthly Call Count:");
         for (Map.Entry<String, Integer> entry : callCountMap.entrySet()) {
             System.out.println("Customer ID: " + entry.getKey()
                     + " → Calls: " + entry.getValue());
