20459f6868d3fa5a41dfc53bbf1d7899b849569f
keshavvyadavv <keshavyadavv21@gmail.com>
2026-01-16 22:13:40 +0530

    [Keshav Yadav]: Added dynamic online marketplace program.

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/dynamiconlinemarketplace/DiscountUtil.java
@@ -0,0 +1,8 @@
+package com.dynamiconlinemarketplace;
+
+public class DiscountUtil {
+	public static <T extends Product<?>> void applyDiscount(T product, double percent) {
+		double discountedPrice = product.getPrice() - (product.getPrice() * percent/100);
+		product.setPrice(discountedPrice);
+	}
+}
