11fe542d4084d344b385789965db5825db6a1f62
keshavvyadavv <keshavyadavv21@gmail.com>
2026-01-27 11:20:52 +0530

    [Keshav Yadav]: Added initial setup

---
+++ java-iostreams-practice/gcr-codebase/JavaIOStreamConcepts/src/com/datahandling/json/Car.java
@@ -0,0 +1,18 @@
+package com.datahandling.json;
+
+public class Car {
+    private String brand;
+    private int year;
+    public Car(String brand, int year) {
+        this.brand = brand;
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
