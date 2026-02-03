381e3f872b6fd4c8d8ef6c0fcede6c7c6bd85217
keshavvyadavv <keshavyadavv21@gmail.com>
2026-02-03 15:57:32 +0530

    [Keshav Yadav]: Added stream questions

--- java8-programming-practice/scenario-based/ScenarioBasedJava8ProgrammingConcepts/src/com/stream/BasicQuestions.java
+++ java8-programming-practice/scenario-based/ScenarioBasedJava8ProgrammingConcepts/src/com/stream/BasicQuestions.java
@@ -155,34 +155,94 @@ public class BasicQuestions {
 		
 		System.out.println(freqq);
 		
+//		  1. Count vowels in a string using Stream
+//			 Input: "programming"
+//			 Output: 3
+		String inputt = "kkkkkkkkkkkkk";
+				
+		int count = (int)inputt.chars()
+		                  .filter(e->e == 'a'||e=='i'|| e == 'o'||e == 'u' || e =='e')
+		                  .count();
+		                  //.orElse(0);
+	    System.out.println(count);
+			
 		
+//		 2. Count number of words in a sentence
+//	    	Input: "Java is very powerful"
+//	    	Output: 4
 		
+	    String sam = "Java is very powerful";
+	    int words = (int)Arrays.stream(sam.trim().split("\\s+")).count();
+	    System.out.println(words);
 		
+	      //3. Find even numbers from list
+	 // 	   Input: [2,5,7,8,10,13]
+	 // 	   Output: [2,8,10]
+		int [] arr = {2,5,7,8,10,13};
+        int[]  resultrrr = Arrays.stream(arr).filter(e -> e%2 == 0).toArray();
+        //System.out.println(Arrays.toString(resultrrr));;
+        
+        //4. Convert list of strings to uppercase
+//           Input: ["java","spring","boot"]
+//        	 Output: ["JAVA","SPRING","BOOT"]
+        
+        String [] arrayy = {"java","spring","boot"};
+        arrayy = Arrays.stream(arrayy).map(e -> e.toUpperCase()).toArray(String[]::new);
+        
+        System.out.println(Arrays.toString(arrayy));
+        
+        //5. Find string length list
+//        Input: ["cat","elephant","dog"]
+//        		Output: [3,8,3]
 		
+        String [] question = {"cat","elephant","dog"};	
+        int [] ans = Arrays.stream(question).mapToInt(String::length).toArray();
+        
+//        System.out.println(Arrays.toString(ans));
+        
+//        
+//        6. Count strings starting with ‘a’
+//        Input: ["apple","banana","ant","car"]
+//        Output: 2
 		
+        String [] ques = {"apple","banana","ant","car"};
+		int an = (int) Arrays.stream(ques).filter(e -> e.startsWith("a")).count();
 		
+		System.out.println(an);
 		
+		//7. Remove empty strings
+//		Input: ["java","","spring","","boot"]
+//				Output: ["java","spring","boot"]
 		
+		String[] strr = {"java","","spring"," k","boot"};
+		String [] rr = Arrays.stream(strr).filter(ss -> ss.trim().length() != 0).toArray(String[]::new);
+//		System.out.println(Arrays.toString(rr));
 		
+		//8. Sum of all numbers using stream
+//			 Input: [1,2,3,4,5]
+//			 Output: 15
 		
+		int [] q = {1,2,3,4,5};
+		int total = Arrays.stream(q).sum();
 		
+		System.out.println(total);
 		
+		//9. Find max number
+//		Input: [10,25,3,99,45]
+//				Output: 99
 		
+		int [] w = {10,25};
+		int max = Arrays.stream(w).max().getAsInt();
+		System.out.println(max);
 		
+//		10. Reverse each string in list
+//		Input: ["java","api"]
+//		Output: ["avaj","ipa"]
 		
+		String [] g = {"java", "api"};
+		String [] as = Arrays.stream(g).map(e -> new StringBuilder(e).reverse().toString()).toArray(String[]::new);
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		System.out.println(Arrays.toString(g));
 		
 		
 		
